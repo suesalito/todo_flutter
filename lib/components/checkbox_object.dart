4640aae19b0686d0397472c029368cf4d2e5f3d8
suesalito <suesalito@gmail.com>
2020-06-29 17:27:43 +0700

    setup stateful widget for ListTile

--- lib/components/checkbox_object.dart
+++ lib/components/checkbox_object.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 
-class CheckBoxObject extends StatelessWidget {
+class CheckBoxObject extends StatefulWidget {
   const CheckBoxObject({Key key, @required this.checkBoxText}) : super(key: key);
 
   final String checkBoxText;
 
   @override
+  _CheckBoxObjectState createState() => _CheckBoxObjectState();
+}
+
+class _CheckBoxObjectState extends State<CheckBoxObject> {
+  bool checkValue = false;
+
+  void checkBoxCallBack(bool value) {
+    setState(() {
+      checkValue = value;
+      print(checkValue);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    bool checkValue = false;
     // return Container(
     //   margin: EdgeInsets.all(5),
     //   child: Row(
@@ -26,8 +39,52 @@ class CheckBoxObject extends StatelessWidget {
     //   ),
     // );
     return ListTile(
-      title: Text(checkBoxText),
-      trailing: Checkbox(value: checkValue, onChanged: null),
+      title: Text(
+        widget.checkBoxText,
+        style: checkValue ? TextStyle(decoration: TextDecoration.lineThrough) : null,
+      ),
+      trailing: TaskCheckBox(
+        checkValue: checkValue,
+        // Below is the Short term if you dont want to declare the call back function.
+        // toggleCallBack: (bool newValue) {
+        //   setState(() {
+        //     checkValue = newValue;
+        //     print(checkValue);
+        //   });
+        // },
+        toggleCallBack: checkBoxCallBack,
+      ),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  const TaskCheckBox({
+    Key key,
+    @required this.checkValue,
+    @required this.toggleCallBack,
+  }) : super(key: key);
+
+  final bool checkValue;
+
+  //below is shot term of the function
+  //final Function toggleCallBack;
+  //below is a type def that can tell update class that it is the callback function that require bool.
+  final ValueChanged<bool> toggleCallBack;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.pink,
+      value: checkValue,
+      hoverColor: Colors.pink,
+      // onChanged: (bool newValue) {
+      //   setState(() {
+      //     checkValue = newValue;
+      //     print(checkValue);
+      //   });
+      // },
+      onChanged: toggleCallBack,
     );
   }
 }
